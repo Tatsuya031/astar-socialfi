10f36f50d49320daf050809a707345815635acce
Tatsuya031 <105437234+Tatsuya031@users.noreply.github.com>
2023-02-01 23:16:47 +0900

    Update connect.tsx

--- hooks/connect.tsx
+++ hooks/connect.tsx
@@ -16,29 +16,25 @@ type Props = {
   setIsSetup: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-// コントラクトとの接続を行うための関数
-export const connectToContract = async (props: Props) => {
+export const connectedApi = async (): Promise<ApiPromise> => {
   // rpcのURL
   const blockchainUrl = "wss://shibuya.public.blastapi.io";
-
-  // この関数でアカウント情報を取得する
-  const extensionSetup = async () => {
-    const { web3Accounts, web3Enable } = await import(
-      "@polkadot/extension-dapp"
-    );
-    const extensions = await web3Enable("Polk4NET");
-    if (extensions.length === 0) {
-      return;
-    }
-    const accounts = await web3Accounts();
-    props.setAccountList(accounts);
-    props.setActingAccount(accounts[0]);
-    props.setIsSetup(true);
-  };
-
-  // この部分でコントラクトに接続
   const wsProvider = new WsProvider(blockchainUrl);
-  const connectedApi = await ApiPromise.create({ provider: wsProvider });
-  props.setApi(connectedApi);
-  await extensionSetup();
+  return await ApiPromise.create({ provider: wsProvider });
+};
+
+// コントラクトとの接続を行うための関数
+export const connectToContract = async (): Promise<
+  InjectedAccountWithMeta[]
+> => {
+  const { web3Accounts, web3Enable } = await import("@polkadot/extension-dapp");
+  const extensions = await web3Enable("Polk4NET");
+
+  if (extensions.length === 0) {
+    return [];
+  }
+
+  const accounts = await web3Accounts();
+
+  return accounts;
 };
